88a11beccc5e6a10a277e47b1b184714aa781f5c
vladnov138 <vladnov138@gmail.com>
2023-09-25 23:01:39 +0800

    Add sqrt task

---
+++ sqrt/main.asm
@@ -0,0 +1,29 @@
+section   .text
+global    main
+
+extern printf
+
+main:
+    finit
+    fld dword [num]
+    fsqrt
+    fstp qword [res]
+
+    push rbp
+    mov rdi, format
+    movq xmm0, qword [res]
+    xor rdx, rdx
+    mov rax, 1
+    call printf
+    pop rbp
+
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
+section   .data
+    num dd 5.0
+    format db "%f", 0xA, 0xD
+
+section .bss
+    res resb 4
